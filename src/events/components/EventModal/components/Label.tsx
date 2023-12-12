bfaf8c97790b1a74cc476600b9be06d6f03ab92d
noveva <nat.solovieva@gmail.com>
2023-12-12 12:02:43 +0100

    Fixed lost style in event form labels

--- src/events/components/EventModal/components/Label.tsx
+++ src/events/components/EventModal/components/Label.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import textVariants from '../../../../common/styles/text';
+import spacingUtils from '../../../../common/styles/spacing';
 
 type Props = {
   text: string;
@@ -20,6 +21,9 @@ function Label({text, nativeID}: Props): React.JSX.Element {
 export default Label;
 
 const styles = StyleSheet.create({
+  container: {
+    ...spacingUtils.paddingT6,
+  },
   text: {
     ...textVariants.heading,
   },
