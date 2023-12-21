3c1e0276d9c8907d5688f8b8330f72bb92d85d45
noveva <nat.solovieva@gmail.com>
2023-12-21 15:04:30 +0100

    Moved add event form into add event modal, event form became redundant

--- src/events/components/EventModal/EventModal.tsx
+++ src/events/components/EventModal/EventModal.tsx
@@ -1,83 +1,155 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {Alert, SafeAreaView, StyleSheet, View} from 'react-native';
-import spacingUtils from '../../../common/styles/spacing';
-import containerUtils from '../../../common/styles/containers';
-import ButtonIcon from '../../../common/components/ButtonIcon';
-import EventForm from './components/EventForm';
+import {Alert, StyleSheet, View} from 'react-native';
+import {getUnixTime} from 'date-fns';
 import {EVENTS_API} from '../../../api/Endpoints';
 import useMutate from '../../../api/useMutate';
 import {RequestStatus} from '../../../api/RequestReducer';
 import {HttpRequestMethods} from '../../../api/utils';
-import {Event} from '../../EventsTypes';
+import spacingUtils from '../../../common/styles/spacing';
+import Button from '../../../common/components/Button';
+import {
+  EVENTS,
+  Event,
+  EventLocation,
+  EventType,
+  LOCATIONS,
+} from '../../EventsTypes';
 import {EventsDispatchContext} from '../../EventsContext';
 import {EventsReducerActionType} from '../../EventsReducer';
-import {iconSize} from '../../../common/styles/iconSize';
+import EventFormGroup from './components/FormGroup';
+
+import {ChipValue, ChipValues} from './components/Chips';
+
+type TimeStamp = {
+  label: string;
+  value: number;
+};
+
+const TIMESTAMPS: TimeStamp[] = [
+  {label: 'now', value: 0},
+  {label: '5m ago', value: 5 * 60},
+  {label: '10m ago', value: 10 * 60},
+  {label: '20m ago', value: 20 * 60},
+  {label: '30m ago', value: 30 * 60},
+  {label: '40m ago', value: 40 * 60},
+  {label: '50m ago', value: 50 * 60},
+  {label: '1h ago', value: 60 * 60},
+];
 
 type Props = {onClose: () => void};
 
 function EventModal({onClose}: Props): React.JSX.Element {
+  const eventTypeOptions: ChipValues<string> = mapOptions(EVENTS);
+  const locationOptions: ChipValues<string> = mapOptions(LOCATIONS);
+  const timestampOptions: ChipValues<number> = TIMESTAMPS.map(
+    ({label, value}) => ({id: value.toString(), label, value}),
+  );
+  const [startTime, setStartTime] = useState(timestampOptions[0].id);
+  const [event, setEvent] = useState<Partial<Event> | undefined>({
+    locationId: locationOptions[0].value as EventLocation,
+    startTimestamp: timestampOptions[0].value,
+  });
   const {status, error, data, mutate} = useMutate<{id: string}>(
     HttpRequestMethods.post,
   );
-  const [eventData, setEventData] = useState<Event>();
   const dispatch = useContext(EventsDispatchContext);
+  const isValidEvent: boolean =
+    !!event &&
+    !!event.eventType &&
+    !!event.locationId &&
+    !!event.startTimestamp &&
+    event.startTimestamp >= 0;
 
-  async function addEvent(eventFormData: Event) {
-    setEventData(eventFormData);
-    await mutate(EVENTS_API.add, eventFormData);
+  function mapOptions(values: readonly string[]) {
+    return values.map(value => ({
+      label: value,
+      id: value,
+      value,
+    }));
+  }
+
+  function updateLocation({value}: ChipValue<EventLocation>) {
+    setEvent({...event, locationId: value} as Event);
+  }
+
+  function updateType({value}: ChipValue<EventType>) {
+    setEvent({...event, eventType: value} as Event);
+  }
+
+  function updateStartTime({id, value}: ChipValue<number>) {
+    const startTimeStamp = getUnixTime(new Date()) - value;
+    setStartTime(id);
+    setEvent({...event, startTimestamp: startTimeStamp} as Event);
+  }
+
+  async function addEvent() {
+    await mutate(EVENTS_API.add, event as Event);
   }
 
   useEffect(() => {
-    if (status === RequestStatus.fetched && dispatch && eventData && data) {
+    if (status === RequestStatus.fetched && dispatch && event && data) {
       // make sure this effect runs only once
-      setEventData(undefined);
+      setEvent(undefined);
       onClose();
       dispatch({
         type: EventsReducerActionType.added,
-        payload: {...eventData, ...data}, //assign event id to form data
+        payload: {...event, ...data} as Event, //assign event id to form data
       });
     }
-  }, [status, onClose, dispatch, eventData, data]);
+  }, [status, onClose, dispatch, event, data]);
 
   if (status === RequestStatus.error) {
     const message = error || 'Something went wrong';
     Alert.alert('Could not create event', message, [
       {text: 'OK', onPress: () => console.log(message)},
     ]);
-    setEventData(undefined);
+    setEvent(undefined);
   }
 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={styles.wrapper}>
-        <ButtonIcon
-          icon="arrow-back"
-          size={iconSize.medium}
-          style={styles.backButton}
-          onPress={onClose}
+    <View style={styles.container}>
+      <EventFormGroup
+        groupId="selectLocation"
+        label="Location"
+        options={locationOptions}
+        selected={event?.locationId}
+        onPress={updateLocation}
+      />
+      <EventFormGroup
+        groupId="selectEventType"
+        label="Event"
+        options={eventTypeOptions}
+        selected={event?.eventType}
+        onPress={updateType}
+      />
+      <EventFormGroup
+        groupId="selectStart"
+        label="Started"
+        selected={startTime}
+        options={timestampOptions}
+        onPress={updateStartTime}
+      />
+      <View style={styles.buttonRow}>
+        <Button
+          label="Submit"
+          disabled={!isValidEvent}
+          waiting={status === RequestStatus.fetching}
+          onPress={addEvent}
         />
-        <EventForm status={status} onSubmit={addEvent} />
       </View>
-    </SafeAreaView>
+    </View>
   );
 }
 
 export default EventModal;
 
 const styles = StyleSheet.create({
-  modal: {
-    ...containerUtils.main,
-    ...spacingUtils.margin0,
+  container: {
+    ...spacingUtils.marginV6,
   },
-  safeArea: {
-    flex: 1,
-  },
-  wrapper: {
-    flex: 1,
-    ...containerUtils.withPadding,
-  },
-  backButton: {
-    alignSelf: 'flex-start',
-    ...spacingUtils.paddingT6,
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    ...spacingUtils.marginV6,
   },
 });
