05dfae0e94800c8fadab260eaaa171f3f85e1ba4
noveva <nat.solovieva@gmail.com>
2023-12-21 12:15:49 +0100

    Fix ts type issue created when switching unix timestamps from custom utils to date-fns

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,5 +1,5 @@
 import {format} from 'date-fns';
 
-export function formatHHmm(datString: number): string {
+export function formatHHmm(datString: number | Date): string {
   return format(new Date(datString), 'HH:mm');
 }
